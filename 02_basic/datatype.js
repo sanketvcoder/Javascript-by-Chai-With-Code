90ba3ef3d029d32b2be99d287446575824b07eb6
Sanket Verma <sanketv403@gmail.com>
2024-08-28 22:33:05 +0530

    this the basic

---
+++ 02_basic/datatype.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") //useto show popups
+// let age = 7
+// if (age < 18) {
+//     throw new Error("sanket")
+// } else {
+//     console.log("success");
+// }
---
+++ 02_basic/let_const_var.js
@@ -0,0 +1,19 @@
+const accountId = 14324
+let accountEmail = "sanketv403@gmail.com"
+var accPassword = "Sanket"
+accountCity = "jaipur"
+let accountState;
+// accountId = 1234
+
+console.log(accountId);
+// so const can't be change 
+
+
+
+accountEmail = "sanket.com"
+accPassword = "1212121"
+accountCity = "kolkata"
+
+console.table([accountId, accPassword, accountEmail, accountCity, accountState])
+
+// we should prefer to use let instead of var
