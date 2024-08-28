499c6c2b0aee0dccbf9d282a7f68470d53424ffd
Sanket Verma <sanketv403@gmail.com>
2024-08-29 00:07:33 +0530

    update this on 29 aug 2024

--- 02_basic/datatype.js
+++ 02_basic/datatype.js
@@ -6,4 +6,23 @@
 //     throw new Error("sanket")
 // } else {
 //     console.log("success");
-// }
+// }k
+
+let isstring  = "333aa"
+let convertToNumber = Number(isstring)
+
+console.log(typeof(convertToNumber))
+console.log(convertToNumber)
+
+
+let dataType = null
+let convertToDatatype = Number(dataType)
+console.log(typeof(convertToDatatype))
+console.log(convertToDatatype)
+
+
+
+
+
+
+
